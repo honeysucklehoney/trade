66ccac6e66b05b844f7161f4c99e9359ecd7c5a2
millieof2017 <61999030+millieof2017@users.noreply.github.com>
2022-02-16 10:34:43 -0500

    gag

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 console.log("hi");
 
-function brick(){
 
+
+function brick(){
   var jjup = document.getElementById('JohnnyJumpUps');
   var brickmaker = document.getElementById('Block1');
-
-      if (jjup.style.display === "none") {
-        // jjup.style.display = "block";
-        // brickmaker.innerHTML = "";
-      }
-      else {
-
 
       jjup.style.display = "none";
       brickmaker.style.color="white";
       brickmaker.style.textShadow="2px 2px 5px black";
       // alert("Good Job :)");
-
-
 
 const para = document.createElement("p");
 const texture = document.createTextNode("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ");
@@ -26,10 +18,17 @@ const element = brickmaker;
 element.appendChild(para);
 
 
-// $("#glean").load(window.location.href+"#glean");
-// location.reload();
+}
 
-      }
+function unbrick() {
+
+var jjup = document.getElementById('JohnnyJumpUps');
+var brickmaker = document.getElementById('Block1');
+var defaultText = 'ma';
+const undo = defaultText
+
+jjup.style.display ="block";
+undo.appendChild(para);
 
 
 }
