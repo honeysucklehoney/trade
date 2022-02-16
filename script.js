85ee166d6745c4797ba5baacd885f58a9c6aaa3f
millieof2017 <61999030+millieof2017@users.noreply.github.com>
2022-02-16 13:13:52 -0500

    bricklaying

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("hi");
 
-
+const para = document.createElement("p");
 
 function brick(){
   var jjup = document.getElementById('JohnnyJumpUps');
@@ -11,7 +11,6 @@ function brick(){
       brickmaker.style.textShadow="2px 2px 5px black";
       // alert("Good Job :)");
 
-const para = document.createElement("p");
 const texture = document.createTextNode("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ");
 para.appendChild(texture);
 const element = brickmaker;
@@ -24,11 +23,47 @@ function unbrick() {
 
 var jjup = document.getElementById('JohnnyJumpUps');
 var brickmaker = document.getElementById('Block1');
-var defaultText = 'ma';
+var defaultText = '';
 const undo = defaultText
 
 jjup.style.display ="block";
-undo.appendChild(para);
+para.innerHTML= defaultText;
 
 
 }
+
+// const para = document.createElement("p");
+//
+// function brick(){
+//   var jjup = document.getElementsByClassName('JohnnyJumpUps');
+//   var brickmaker = document.getElementsByClassName('Block')
+//   var i=brickmaker.length;
+//   var y=jjup.length;
+//
+//       jjup[y].style.display="none";
+//       brickmaker[i].style.color="white";
+//       brickmaker[i].style.textShadow="2px 2px 5px black";
+//       // alert("Good Job :)");
+//
+// const texture = document.createTextNode("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ");
+// para.appendChild(texture);
+// const element = brickmaker[i];
+// element.appendChild(para);
+//
+//
+// }
+//
+// function unbrick() {
+//
+//   var jjup = document.getElementsByClassName('JohnnyJumpUps');
+//   var brickmaker = document.getElementsByClassName('Block')
+//   var i=brickmaker.length;
+//   var y=jjup.length;
+//   var defaultText = '';
+// const undo = defaultText
+//
+// jjup[y].style.display="block";
+// para.innerHTML= defaultText;
+//
+//
+// }
